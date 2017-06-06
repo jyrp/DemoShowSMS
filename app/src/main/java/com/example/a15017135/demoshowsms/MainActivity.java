21a5d7798af6440941724512043408b2d1b84b0e
15017135 <15017135@myrp.edu.sg>
2017-06-06 11:38:39 +0800

    commit

--- app/src/main/java/com/example/a15017135/demoshowsms/MainActivity.java
+++ app/src/main/java/com/example/a15017135/demoshowsms/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.a15017135.demoshowsms;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
         btnRetrieve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+                int permissionCheck = PermissionChecker.checkSelfPermission
+                        (MainActivity.this, Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission not
+                    //  granted yet
+                    return;
+                }
+
                 // Create all messages URI
                 Uri uri = Uri.parse("content://sms");
                 // The columns we want
@@ -60,5 +78,27 @@ public class MainActivity extends AppCompatActivity {
                 tvSms.setText(smsBody);
             }
         });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the read SMS
+                    //  as if the btnRetrieve is clicked
+                    btnRetrieve.performClick();
+
+                } else {
+                    // permission denied... notify user
+                    Toast.makeText(MainActivity.this, "Permission not granted",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
